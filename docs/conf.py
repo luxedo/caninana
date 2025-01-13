453bdd201a4e41aefdc677482124cabbe701a01b
Luiz Eduardo Amaral <luizamaral306@gmail.com>
2025-01-12 22:58:56 -0300

    DOC: add sphinx automodule

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,16 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../src"))
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version("lontras")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "unknown"
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -9,12 +19,15 @@
 project = "lontras"
 copyright = "2025, Luiz Eduardo Amaral"
 author = "Luiz Eduardo Amaral"
-release = "0.0.1"
+release = __version__
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx_rtd_theme",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = []
@@ -23,5 +36,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
--- docs/index.rst
+++ docs/index.rst
@@ -3,14 +3,18 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Lontras documentation
-=====================
-
-Add your content using ``reStructuredText`` syntax. See the
-`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
-documentation for details.
-
+Lontras |version| documentation
+===============================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
+
+   modules
+
+Indices and tables
+==================
+
+*   :ref:`genindex`
+*   :ref:`modindex`
+*   :ref:`search`
